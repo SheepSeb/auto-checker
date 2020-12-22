0dc653db790cda539990c42448b7408a6f7c2b06
Cristiana Andrei <cristiana699@gmail.com>
2020-12-23 01:06:59 +0200

    Added function to write the input from console into a file

--- CheckerSolution/src/test/java/MainTest.java
+++ CheckerSolution/src/test/java/MainTest.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MainTest {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,9 @@ public class MainTest {
         File secondFile = new File();
 
         //Doesn't go to the path gets IOException
-        myFile.writeInFile("src/test/file_testing/text_crt.txt");
+        //myFile.writeInFile("src/test/file_testing/text_crt.txt");
+
+        System.out.println(myFile.inputToFile());
 
     }
 }
