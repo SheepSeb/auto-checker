dc10759d813b42a4648ce0f9d9c2c841d486b0a8
Remus <remus.dan@stud.fils.upb.ro>
2020-12-24 00:30:07 +0200

    am avut probleme cu parametrul prin care trimit path-ul pentru a crea un file in main.

--- CheckerSolution/src/main/java/File_Check.java
+++ CheckerSolution/src/main/java/File_Check.java
@@ -1,15 +1,46 @@
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
 
 public class File_Check {
 
-    /** Variabilele predefinite
+    /**
+     * Variabilele predefinite
      * Puteti modifica ce vreti de aici cat timp functioneaz
-     * astea sunt doar niste indicatii*/
+     * astea sunt doar niste indicatii
+     */
 
     private int numberFiles;
     private File dirOutput;
     private File dirCorrect;
-//asdas
 
+    public File_Check(File a, File b, int nrFiles) {
+        this.numberFiles = nrFiles;
+        this.dirCorrect = b;
+        this.dirOutput = a;
+    }
+
+    public static boolean sameContent(File a, File b) throws IOException {
+        return FileUtils.contentEquals(a, b);
+    }
+
+    //functia iti spune cate din fisierele din directorul de output
+    // au aceleasi continut cu cele din directorul cu corecte
+    public int howManyAreCorrect() throws IOException {
+        int correct = 0;
+        String x = dirOutput.getAbsolutePath();
+        String y = dirOutput.getAbsolutePath();
+        for (int i = 0; i < numberFiles; i++)
+        {
+            File fileOut = new File(x + String.valueOf(i) + ".txt");//modificat .out cu .txt
+            File fileCor = new File(y + String.valueOf(i) + ".txt");//pt testare
+
+            if (sameContent(fileOut,fileCor))
+                correct++;
+        }
+        return correct;
+    }
 
 }
+
--- CheckerSolution/src/test/java/MainTest.java
+++ CheckerSolution/src/test/java/MainTest.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 
 public class MainTest {
     public static void main(String[] args) throws IOException {
-        File myFile = new File();
-        File secondFile = new File();
+        File myFile = new File("C:/Users/remus/Desktop/TestDir1");
+        File secondFile = new File("C:/Users/remus/Desktop/TestDir2");
+
 
         //Doesn't go to the path gets IOException
-        myFile.writeInFile("src/test/file_testing/text_crt.txt");
+       // myFile.writeInFile("src/test/file_testing/text_crt.txt");
 
     }
 }
