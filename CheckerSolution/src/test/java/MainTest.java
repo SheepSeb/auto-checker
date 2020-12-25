58d440e6b47ec6d44a9d9549d80647a6e9a09f29
Anca <anca_ciolmec@yahoo.com>
2020-12-24 18:51:50 -0800

    Anca

--- CheckerSolution/src/test/java/MainTest.java
+++ CheckerSolution/src/test/java/MainTest.java
@@ -1,14 +1,6 @@
-
-
 import java.io.IOException;
 
 public class MainTest {
-    public static void main(String[] args) throws IOException {
-        File myFile = new File();
-        File secondFile = new File();
-
-        //Doesn't go to the path gets IOException
-        myFile.writeInFile("src/test/file_testing/text_crt.txt");
 
     }
-}
+
