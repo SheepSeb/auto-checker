4b9122eb3542584257a48669d6572e1e6b50313e
Anca <anca_ciolmec@yahoo.com>
2020-12-24 19:09:51 -0800

    Anca

--- CheckerSolution/src/main/java/Output_files.java
+++ CheckerSolution/src/main/java/Output_files.java
@@ -1,22 +1,53 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 public class Output_files {
 
-    /** Variabilele predefinite
-     * Puteti modifica ce vreti de aici cat timp functioneaz
-     * astea sunt doar niste indicatii*/
+        static void createFolder(File folder) {
+            if (!folder.exists()) {
+                folder.mkdir();
+                System.out.println("Folder has been created");
+            } else {
+                System.out.println("Folder already exists");
+            }
+        }
 
-    private String dirName;
-    private int numberFiles = 0;
-    private String name;
-    private File outputDir;
+        static void createFiles(int testsNumber) {
+            for (int testCount = 1; testCount <= testsNumber; testCount++) {
+                File file = new File("Output/" + testCount + "_test.out");
+                try {
+                    PrintWriter output = new PrintWriter(file);
+                    output.close();
+                    System.out.println("am creat");
+                } catch (IOException createFolderOutputEx) {
+                    System.out.printf("ERROR: %s/n", createFolderOutputEx);
+                }
+            }
 
-    /** Constructor primeste ca parametru un string
-     * ce este destinatia abosoluta a directorului/folder
-     * creeaza un File (a nu se intelege ca deja este facut in sistem file-ul)*/
+        }
 
-    public Output_files(String dirName) {
-        this.dirName = dirName;
-        outputDir = new File(dirName);
+        static void deleteFolder(File folder) {
+            //Delete every file in the folder
+            for (File subFile : folder.listFiles()) {
+                if (subFile.isFile()) {
+                    System.out.println("Am ce sa sterg");
+                    if (subFile.delete()) {
+                        System.out.println("Deleted the file: " + subFile.getName());
+                    } else {
+                        System.out.println("Failed to delete the file.");
+                    }
+                }
+            }
+            //If the folder is empty, delete it
+            if (folder.isDirectory()) {
+                if (folder.delete()) {
+                    System.out.println("Deleted the folder: " + folder.getName());
+                } else {
+                    System.out.println("Failed to delete the folder.");
+                }
+            }
+        }
     }
-}
+
--- CheckerSolution/src/test/java/MainTest.java
+++ CheckerSolution/src/test/java/MainTest.java
@@ -1,6 +1,22 @@
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainTest {
+    public static void main(String[] args) throws IOException {
+        //create Folder
+        File folder = new File("Output");
+        Output_files.createFolder(folder);
+
+        //create n (given from keyboard) files
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter number of tests:");
+        int testsNumber = console.nextInt();
+        Output_files.createFiles(testsNumber);
+
+        //Delete n files, and then the folder
+        Output_files.deleteFolder(folder);
+
 
     }
-
+}
